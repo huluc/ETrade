4fc5132fcb5d9f4c14b857ff526999a563974ee5
huluc <40721928+huluc@users.noreply.github.com>
2019-02-20 22:24:28 +0300

    - Add Cart class.

---
+++ ETrade.Entity/Concrete/Cart.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETrade.Entity.Abstract;
+
+namespace ETrade.Entity.Concrete
+{
+    public class Cart : IEntity
+    {
+        List<CartLine> _lines = new List<CartLine>();
+
+        public void AddToCart(Product product, int quantity)
+        {
+            CartLine cartLine = _lines.FirstOrDefault(c => c.Product.ProductId == product.ProductId);
+            if (cartLine == null)
+            {
+                _lines.Add(new CartLine { Product = product, Quantity = quantity });
+            }
+            else
+            {
+                cartLine.Quantity += quantity;
+            }
+        }
+
+        public void RemoveFromCart(Product product)
+        {
+            _lines.RemoveAll(line => line.Product.ProductId == product.ProductId);
+        }
+
+        public decimal Total
+        {
+            get { return _lines.Sum(line => line.Product.UnitPrice * line.Quantity); }
+        }
+
+        public void Clear()
+        {
+            _lines.Clear();
+        }
+
+        public List<CartLine> Lines
+        {
+            get { return _lines; }
+        }
+
+    }
+
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ ETrade.MvcWebUI/Controllers/CartController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+using ETrade.Entity.Concrete;
+using ETrade.Interfaces;
+
+namespace ETrade.MvcWebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IProductService _productService;
+
+        public CartController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public ActionResult AddToCart(int productId) //formdan post edilen veriyi 'productId' map eder.
+        {
+            Product product = _productService.GetById(productId);
+
+            Cart cart = (Cart)Session["cart"]; //if the cart doesn't exist in the session. We created it. Otherwise We increased quantity of the added product in the cart. 
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["cart"] = cart;
+            }
+
+            cart.AddToCart(product, 1);
+            return View("Index", cart);
+        }
+
+        public ActionResult RemoveFromCart(int productId)
+        {
+            Product product = _productService.GetById(productId);
+            Cart cart = (Cart) Session["Cart"];
+            cart.RemoveFromCart(product);
+            return View("Index", cart);
+        }
+    }
+}
